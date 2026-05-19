b7691f317d1dea3be0b43329b0345ab4e2c9676d
Nikita Dzemidovich <grodnonik2005@gmail.com>
2026-05-19 21:07:40 +0300

    chore(init): initialize nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import Aura from '@primeuix/themes/aura';
+
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    css: ['./app/assets/css/main.css'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ]
+    },
+    modules: ['@pinia/nuxt', '@nuxt/eslint', '@primevue/nuxt-module'],
+    primevue: {
+        options: {
+            theme: {
+                preset: Aura
+            }
+        }
+    }
+})
